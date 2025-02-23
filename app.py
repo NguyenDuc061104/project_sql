e326d8e96c8aee462f5b0941014a7c3b7dcb8e52
Nguyễn An Đức <anduc061104@gmail.com>
2025-02-23 23:07:49 +0700

    commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import dash
+from dash import html
+import dash_bootstrap_components as dbc
+from layout import create_layout
+from callbacks import register_callbacks
+
+# Khởi tạo Dash app
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# Gán layout
+app.layout = create_layout()
+
+# Đăng ký callback
+register_callbacks(app)
+
+# Thêm server để Gunicorn có thể chạy
+server = app.server
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
